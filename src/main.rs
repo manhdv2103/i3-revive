dc095f11918fc45688f9ab4584ee2b0a36f77562
Manh Vu <manhdv2103@gmail.com>
2025-07-23 17:48:24 +0700

    add argument commands support

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,41 @@ use i3_revive::{
     i3ipc::{connect_i3, get_tree},
     process::save_processes,
 };
+use std::env;
 
 fn main() {
-    load_config();
-    let mut stream = connect_i3().expect("Failed to connect to i3");
-    let root = get_tree(&mut stream).expect("Failed to get tree");
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() != 2 {
+        eprintln!("Usage: {} <save|restore|rm>", args[0]);
+        std::process::exit(1);
+    }
+    
+    let command = &args[1];
+    
+    match command.as_str() {
+        "save" => {
+            load_config();
+            let mut stream = connect_i3().expect("Failed to connect to i3");
+            let root = get_tree(&mut stream).expect("Failed to get tree");
 
-    let workspaces = find_workspaces(root);
-    let windows = get_all_windows(&workspaces);
+            let workspaces = find_workspaces(root);
+            let windows = get_all_windows(&workspaces);
 
-    save_workspaces(workspaces);
-    save_processes(windows);
+            save_workspaces(workspaces);
+            save_processes(windows);
+        }
+        "restore" => {
+            eprintln!("TODO: restore command not implemented yet");
+            std::process::exit(1);
+        }
+        "rm" => {
+            eprintln!("TODO: rm command not implemented yet");
+            std::process::exit(1);
+        }
+        _ => {
+            eprintln!("Usage: {} <save|restore|rm>", args[0]);
+            std::process::exit(1);
+        }
+    }
 }
